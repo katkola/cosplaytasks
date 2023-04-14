429d734a531915c1c93725f9b17096583d83d0d9
katkola <katkola@gmail.com>
2023-04-14 13:02:40 -0400

    dasd

--- server/models/item.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const itemSchema = new mongoose.Schema({
-    title: String,
-    image: String
-}, { timestamps: true })
-const Item = mongoose.model('Item', itemSchema);
