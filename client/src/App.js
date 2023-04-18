02d6fae837f9c277336f29aad91c79f96afa93e7
Kat Hogan <32649279+katkola@users.noreply.github.com>
2023-04-18 03:42:25 -0400

    token and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "../src/components/Main";
 import ViewCosplay from './components/ViewCosplay';
 import AllTasks from './components/AllTasks';
+import Login from './components/Users/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token){
+    <Login setToken={setToken}/>
+  }
   return (
     <div className="App">
       <div className="App">
       <BrowserRouter>
         <Routes>
           <Route element={<Main/>} path="/" />
+          <Route element={<Login/>} path="/dashboard"/>
           <Route element={<AllTasks/>} path="/test/tasks" />
           <Route element={<ViewCosplay/>} path="cosplays/view/:id"/>
         </Routes>
